4592829294974d4d9abe02e05f3a857d053344bc
Gabriel <mfkrgabriel@gmail.com>
2017-11-15 17:52:58 -0200

    Provisório

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
-var kappapride = 'Hello World';
+// variáveis imutáveis
+var calcular = document.querySelector("#calcular"); //Botão
 
-function kappa() {
-  return kappapride;
+var sonho = document.querySelector("#sonho");
+var salario = document.querySelector("#salario");
+var gasto = document.querySelector("#gasto");
+var resultado = document.querySelector("#resultado");
+
+// variáveis mutáveis;
+var liquidoMensal = 0;
+
+function calculoFinal() {
+  var tempoMeses = 0;
+  var tempoAnos = 0;
+
+  if (salario.value > 0 && sonho.value > 0 && gasto.value > 0) {
+    liquidoMensal = salario.value - gasto.value;
+    tempoMeses = Math.ceil(sonho.value / liquidoMensal);
+    if (tempoMeses > 12) {
+      tempoAnos = Math.floor(tempoMeses / 12);
+      tempoMeses -= tempoAnos * 12;
+    }
+    if (liquidoMensal > 0) {
+      resultado.innerHTML = "Se voc\xEA economizar " + liquidoMensal + " por m\xEAs, em " + (tempoAnos > 0 ? tempoAnos + " anos e " + tempoMeses + " meses" : tempoMeses + " meses") + " voc\xEA ter\xE1 o suficiente para realizar seu sonho, e ainda sobrar\xE1 " + ((tempoAnos * 12 + tempoMeses) * liquidoMensal - sonho.value) + ".";
+    } else {
+      resultado.innerHTML = "Voc\xEA gasta mais que ganha :c";
+    }
+  } else {
+    resultado.innerHTML = "Nenhum valor pode ser negativo.";
+  }
 }
 
-kappa();
+calcular.onclick = calculoFinal;
--- source/main.js
+++ source/main.js
@@ -1,7 +1,41 @@
-const kappapride = 'Hello World';
+// variáveis imutáveis
+const calcular = document.querySelector("#calcular"); //Botão
 
-function kappa() {
-  return kappapride;
+const sonho = document.querySelector("#sonho");
+const salario = document.querySelector("#salario");
+const gasto = document.querySelector("#gasto");
+const resultado = document.querySelector("#resultado");
+
+// variáveis mutáveis;
+let liquidoMensal = 0;
+
+function calculoFinal() {
+  let tempoMeses = 0;
+  let tempoAnos = 0;
+
+  if (salario.value > 0 && sonho.value > 0 && gasto.value > 0) {
+    liquidoMensal = salario.value - gasto.value;
+    tempoMeses = Math.ceil(sonho.value / liquidoMensal);
+    if (tempoMeses > 12) {
+      tempoAnos = Math.floor(tempoMeses / 12);
+      tempoMeses -= tempoAnos * 12;
+    }
+    if (liquidoMensal > 0) {
+      resultado.innerHTML = `Se você economizar ${liquidoMensal} por mês, em ${
+        tempoAnos > 0
+          ? tempoAnos + " anos e " + tempoMeses + " meses"
+          : tempoMeses + " meses"
+      } você terá o suficiente para realizar seu sonho, e ainda sobrará ${(tempoAnos *
+        12 +
+        tempoMeses) *
+        liquidoMensal -
+        sonho.value}.`;
+    } else {
+      resultado.innerHTML = `Você gasta mais que ganha :c`;
+    }
+  } else {
+    resultado.innerHTML = `Nenhum valor pode ser negativo.`;
+  }
 }
 
-kappa();
+calcular.onclick = calculoFinal;
