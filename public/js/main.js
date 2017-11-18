90bdd1697262d77ca6aa6f0cfb6d30e699d6f63f
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2017-11-17 22:35:18 -0200

    Correção do erro quando a quantidade de meses é 0, bem como do erro com valor restante 0.

--- public/js/main.js
+++ public/js/main.js
@@ -14,16 +14,34 @@ var liquidoMensal = 0;
 function calculoFinal() {
   var tempoMeses = 0;
   var tempoAnos = 0;
+  var strResultado = '';
+  var valorRestante = 0;
 
   if (salario.value > 0 && sonho.value > 0 && gasto.value > 0) {
     liquidoMensal = salario.value - gasto.value;
     tempoMeses = Math.ceil(sonho.value / liquidoMensal);
+    valorRestante = liquidoMensal * tempoMeses - sonho.value;
     if (tempoMeses > 12) {
       tempoAnos = Math.floor(tempoMeses / 12);
       tempoMeses -= tempoAnos * 12;
     }
     if (liquidoMensal > 0) {
-      resultado.innerHTML = 'Se voc\xEA economizar R$ ' + liquidoMensal + ' por m\xEAs, em ' + (tempoAnos > 0 ? tempoAnos + ' ano(s) e ' + tempoMeses + ' mes(es)' : tempoMeses + ' mes(es)') + ' voc\xEA ter\xE1 o suficiente para realizar seu sonho, e ainda sobrar\xE1 R$ ' + ((tempoAnos * 12 + tempoMeses) * liquidoMensal - sonho.value) + '.';
+      strResultado = 'Se voc\xEA economizar R$ ' + liquidoMensal + ' por m\xEAs, em ';
+      if (tempoAnos > 0) {
+        strResultado = strResultado & tempoAnos + ' anos ';
+        if (tempoMeses > 0) {
+          strResultado = strResultado & 'e ' + tempoMeses + ' meses ';
+        }
+      } else if (tempoMeses > 0) {
+        strResultado = strResultado & tempoMeses + ' meses ';
+      }
+      strResultado = strResultado & 'você terá o suficiente para realizar seu sonho';
+      if (valorRestante > 0) {
+        strResultado = strResultado & ', e ainda sobrar\xE1 R$ ' + valorRestante + '!';
+      } else {
+        strResultado = strResultado & '!';
+      }
+      resultado.innerHTML = strResultado;
     } else {
       resultado.innerHTML = 'Você gasta mais que ganha :c';
     }
